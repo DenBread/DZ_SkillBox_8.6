9a4b2b32ff80e39007471caf6bf76848afa210c6
DenBread <37297335+DenBread@users.noreply.github.com>
2021-09-09 19:24:13 +0300

    End

---
+++ DZ_SkillBox_8.6/Assets/Scripts/MoveLoop.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLoop : MonoBehaviour, IMovevarble
+{
+    [SerializeField] private float _speed; // скорость кубика
+    [SerializeField] private List<Vector3> _vec; // точки перемещения куба
+    [SerializeField] private int _indexVec; // индекс точки перемещения
+    [SerializeField] private GameObject[] _gm; // кубы для эстафеты
+
+    private void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Метод для перемещения данных по индексу меня их местами в list
+    /// </summary>
+    /// <typeparam name="T">тип list</typeparam>
+    /// <param name="list"></param>
+    /// <param name="i">первый параметр по индексу</param>
+    /// <param name="j">второй параметр по индесу</param>
+    public static void Swap<T>(List<T> list, int i, int j)
+    {
+        var elem1 = list[i];
+        var elem2 = list[j];
+
+        list[i] = elem2;
+        list[j] = elem1;
+    }
+
+    /// <summary>
+    /// Метод для передачи эстафеты кубов
+    /// </summary>
+    public void Move()
+    {
+        _gm[_indexVec].transform.position = Vector3.MoveTowards(_gm[_indexVec].transform.position, _vec[_indexVec],
+            _speed * Time.deltaTime);
+
+        if (_gm[_indexVec].transform.position == _vec[_indexVec])
+        {
+            _indexVec++;
+            if (_indexVec == _vec.Count)
+            {
+                //Debug.Log("Цикл вкл")
+                _indexVec = 0;
+                for (int i = 0; i < _vec.Count; i++)
+                {
+                    _indexVec++;
+                    if (_indexVec < _vec.Count)
+                    {
+                        Swap(_vec, i, _indexVec);
+                    }
+                }
+                _indexVec = 0;
+            }
+
+        }
+    }
+}
---
+++ DZ_SkillBox_8.6/Assets/Scripts/MovePingPonk.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePingPonk : MonoBehaviour, IMovevarble
+{
+    private GameObject _cube; // сам куб для перемещения
+    private float _speed; // скорость перемещения
+    [SerializeField] private Vector3[] _vectorTartget; // точки для перемещния куба
+    [SerializeField] private int _indexTaget; // индекс точки перемещения
+    [SerializeField] private bool forward;
+
+    private void Start()
+    {
+        _cube = gameObject;
+        _speed = 7f;
+        _indexTaget = 1;
+        forward = true;
+
+    }
+        
+    private void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Метод для выполнения перемещения обьекта, как пинг-понг
+    /// </summary>
+    public void Move()
+    {
+        _cube.transform.position = Vector3.MoveTowards(transform.position, _vectorTartget[_indexTaget], _speed * Time.fixedDeltaTime);
+
+        if (transform.position == _vectorTartget[_indexTaget] && _indexTaget < _vectorTartget.Length && forward)
+        {
+            _indexTaget++;
+            if (_indexTaget == _vectorTartget.Length)
+            {
+                forward = false;
+                _indexTaget--;
+            }
+        }
+
+        if (transform.position == _vectorTartget[_indexTaget] && !forward)
+        {
+            _indexTaget--;
+            if (_indexTaget <= -1)
+            {
+                forward = true;
+                _indexTaget++;
+            }
+        }
+    }
+}
